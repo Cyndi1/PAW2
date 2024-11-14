80760a47b8031d2a27466ccdbb2b2f711eab349a
Cyndi1 <cyndiclaudiawang_2226240125@mhs.mdp.ac.id>
2024-11-14 13:05:28 +0700

    lanjutan prjBuku

--- prjbuku/src/app/app.module.ts
+++ prjbuku/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from '../admin/admin.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { BukuComponent } from '../buku/buku.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    BukuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
